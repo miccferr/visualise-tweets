1987e7ac969e0da0670f76dfbfd30522af45e0b3
Michele Ferretti <mic.ferretti@gmail.com>
2017-03-24 17:09:51 +0000

    cleaned repo

--- client/src/Components/MyMap.js
+++ client/src/Components/MyMap.js
@@ -1,12 +1,6 @@
 import React from 'react';
 import {Map, CircleMarker, Popup, TileLayer} from 'react-leaflet';
 import './MyMap.css'
-import supercluster from 'supercluster'
-let index = supercluster({
-    radius: 40,
-    maxZoom: 16
-});
-
 
 const MyMap = (props) => (
   <Map center={props.center} zoom={13} id='map' className='z-0'>
