20d5cc66105e61f2eff782d5dfabeeb37a291278
Michele Ferretti <mic.ferretti@gmail.com>
2017-03-24 19:43:10 +0000

    reverted to before ejection

--- client/src/Components/MyMap.js
+++ client/src/Components/MyMap.js
@@ -2,12 +2,10 @@ import React from 'react';
 import {Map, CircleMarker, Popup, TileLayer} from 'react-leaflet';
 import './MyMap.css'
 
-
-const myUrl = "http://api.tiles.mapbox.com/v4/<mapid>/{z}/{x}/{y}.png?access_token=" + process.env.MAPBOX_ACCESS_TOKEN
 const MyMap = (props) => (
   <Map center={props.center} zoom={13} id='map' className='z-0'>
     <TileLayer
-      url= {myUrl}
+      url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
       attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'/>      
 
 
@@ -30,7 +28,6 @@ const MyMap = (props) => (
 
       // {index.getClusters([-180, -85, 180, 85], 2)}
 export default MyMap;
-
 
 
 
